15d86b8ef69de1b3d0190f6431523715a9c2bc85
ABHI-PATEL1812 <53005111+ABHI-PATEL1812@users.noreply.github.com>
2019-11-19 04:32:52 +0530

    login n signup layout added

--- app/src/main/java/com/example/applyforjobs/Signup_page.java
+++ app/src/main/java/com/example/applyforjobs/Signup_page.java
@@ -11,29 +11,28 @@ import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class MainActivity extends AppCompatActivity {
+public class Signup_page extends AppCompatActivity {
 
-    ImageView imageview1, imageview2;
-    TextInputEditText email, password;
-    MaterialButton loginbtn;
-    TextView sigup;
-    ProgressBar progressbar;
+    ImageView imageview1,imageview2;
+    TextInputEditText fullname,phonenumber,email,password;
+    MaterialButton signupbtn;
+    ProgressBar progressBar;
+    TextView login;
     FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_signup_page);
         imageview1 = findViewById(R.id.imageView1);
-        imageview2 = findViewById(R.id.imageView2);
+        imageview1 = findViewById(R.id.imageView2);
+        fullname = findViewById(R.id.fullname);
+        phonenumber = findViewById(R.id.phonenumber);
         email = findViewById(R.id.email);
         password = findViewById(R.id.password);
-        loginbtn = findViewById(R.id.login);
-        sigup = findViewById(R.id.signup);
-        progressbar = findViewById(R.id.progressBar);
+        signupbtn = findViewById(R.id.signup);
+        progressBar = findViewById(R.id.progressBar);
+        login = findViewById(R.id.login);
         firebaseAuth = firebaseAuth.getInstance();
     }
-
 }
-
-
