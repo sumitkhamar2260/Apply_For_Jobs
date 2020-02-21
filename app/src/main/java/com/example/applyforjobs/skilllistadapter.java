0b23f936aeb5d43b67b2b02a554b29d8d38a86e1
ABHI-PATEL1812 <53005111+ABHI-PATEL1812@users.noreply.github.com>
2020-02-21 11:25:27 +0530

    skill listview added

---
+++ app/src/main/java/com/example/applyforjobs/skilllistadapter.java
@@ -0,0 +1,69 @@
+package com.example.applyforjobs;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class skilllistadapter extends BaseAdapter implements ListAdapter {
+
+    private final ArrayList<String> list;
+    private Context context;
+
+    public skilllistadapter(ArrayList<String> list, Context context) {
+        this.list = list;
+        this.context = context;
+    }
+
+
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public Object getItem(int pos) {
+        return list.get(pos);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        View view = convertView;
+        if (view == null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.listview_skill, null);
+        }
+
+        //Handle TextView and display string from your list
+        TextView listItemText = (TextView)view.findViewById(R.id.list_item_string);
+        listItemText.setText(list.get(position));
+
+        //Handle buttons and add onClickListeners
+        ImageView deleteBtn = (ImageView) view.findViewById(R.id.delete_btn);
+
+
+        deleteBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                //do something
+                list.remove(position); //or some other task
+                notifyDataSetChanged();
+            }
+        });
+        return view;
+    }
+}
